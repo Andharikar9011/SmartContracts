8555e2ca9050f37cab499a4d93b01bd559de92a4
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2023-03-13 20:52:24 +0530

    loops basic

---
+++ Loops.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract CheckPrime{
+    function checkprime(uint a) external pure returns(bool){
+        if(a < 2){
+            return false;
+        }else{
+            for(uint i = 2; i < a; i++){
+                if (a % i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    
+}
